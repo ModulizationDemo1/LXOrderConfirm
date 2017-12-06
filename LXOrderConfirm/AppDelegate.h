1c763326e9880a242c10e69f4eca1c3a2d0705a7
Ace <ohlaixixi@163.com>
2017-12-06 10:06:33 +0800

    no message

---
+++ LXOrderConfirm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXOrderConfirm
+//
+//  Created by LX on 2017/12/6.
+//  Copyright © 2017年 KinRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXOrderConfirm/Classes/LXOrderConfirmController.h
@@ -0,0 +1,13 @@
+//
+//  LXOrderConfirmController.h
+//  LXOrderConfirm
+//
+//  Created by LX on 2017/12/6.
+//  Copyright © 2017年 KinRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXOrderConfirmController : UIViewController
+
+@end
